2826af739c32b840055032d8488f0676e31790c7
Torin Hawkins <thawkins022713@students.iticollege.edu>
2021-06-06 10:28:17 -0500

    Adding remaing portion on copied code

---
+++ MailingListApp/Form1.Designer.cs
@@ -0,0 +1,295 @@
+﻿
+namespace MailingListApp
+{
+    partial class frmMailingList
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.grpMail = new System.Windows.Forms.GroupBox();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnAccept = new System.Windows.Forms.Button();
+            this.txtZip = new System.Windows.Forms.TextBox();
+            this.txtState = new System.Windows.Forms.TextBox();
+            this.txtCity = new System.Windows.Forms.TextBox();
+            this.txtAddress = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.grpTime = new System.Windows.Forms.GroupBox();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnPause = new System.Windows.Forms.Button();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.txtElapsedTime = new System.Windows.Forms.TextBox();
+            this.timSeconds = new System.Windows.Forms.Timer(this.components);
+            this.grpMail.SuspendLayout();
+            this.grpTime.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // grpMail
+            // 
+            this.grpMail.Controls.Add(this.btnClear);
+            this.grpMail.Controls.Add(this.btnAccept);
+            this.grpMail.Controls.Add(this.txtZip);
+            this.grpMail.Controls.Add(this.txtState);
+            this.grpMail.Controls.Add(this.txtCity);
+            this.grpMail.Controls.Add(this.txtAddress);
+            this.grpMail.Controls.Add(this.txtName);
+            this.grpMail.Controls.Add(this.label5);
+            this.grpMail.Controls.Add(this.label4);
+            this.grpMail.Controls.Add(this.label3);
+            this.grpMail.Controls.Add(this.label2);
+            this.grpMail.Controls.Add(this.label1);
+            this.grpMail.Enabled = false;
+            this.grpMail.Location = new System.Drawing.Point(48, 47);
+            this.grpMail.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.grpMail.Name = "grpMail";
+            this.grpMail.Padding = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.grpMail.Size = new System.Drawing.Size(581, 337);
+            this.grpMail.TabIndex = 0;
+            this.grpMail.TabStop = false;
+            this.grpMail.Text = "Address Information";
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(274, 285);
+            this.btnClear.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(86, 31);
+            this.btnClear.TabIndex = 11;
+            this.btnClear.Text = "&Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnAccept
+            // 
+            this.btnAccept.Location = new System.Drawing.Point(115, 285);
+            this.btnAccept.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnAccept.Name = "btnAccept";
+            this.btnAccept.Size = new System.Drawing.Size(86, 31);
+            this.btnAccept.TabIndex = 5;
+            this.btnAccept.Text = "&Accept";
+            this.btnAccept.UseVisualStyleBackColor = true;
+            this.btnAccept.Click += new System.EventHandler(this.btnAccept_Click);
+            // 
+            // txtZip
+            // 
+            this.txtZip.Location = new System.Drawing.Point(401, 220);
+            this.txtZip.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtZip.Name = "txtZip";
+            this.txtZip.Size = new System.Drawing.Size(146, 27);
+            this.txtZip.TabIndex = 4;
+            // 
+            // txtState
+            // 
+            this.txtState.Location = new System.Drawing.Point(115, 220);
+            this.txtState.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtState.Name = "txtState";
+            this.txtState.Size = new System.Drawing.Size(206, 27);
+            this.txtState.TabIndex = 3;
+            // 
+            // txtCity
+            // 
+            this.txtCity.Location = new System.Drawing.Point(115, 165);
+            this.txtCity.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtCity.Name = "txtCity";
+            this.txtCity.Size = new System.Drawing.Size(431, 27);
+            this.txtCity.TabIndex = 2;
+            // 
+            // txtAddress
+            // 
+            this.txtAddress.Location = new System.Drawing.Point(115, 109);
+            this.txtAddress.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtAddress.Name = "txtAddress";
+            this.txtAddress.Size = new System.Drawing.Size(431, 27);
+            this.txtAddress.TabIndex = 1;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(115, 55);
+            this.txtName.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(431, 27);
+            this.txtName.TabIndex = 0;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(341, 231);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(31, 20);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Zip";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(34, 231);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(43, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "State";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(34, 169);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(34, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "City";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(34, 109);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Address";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(34, 59);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Name";
+            // 
+            // grpTime
+            // 
+            this.grpTime.Controls.Add(this.btnExit);
+            this.grpTime.Controls.Add(this.btnPause);
+            this.grpTime.Controls.Add(this.btnStart);
+            this.grpTime.Controls.Add(this.txtElapsedTime);
+            this.grpTime.Location = new System.Drawing.Point(657, 47);
+            this.grpTime.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.grpTime.Name = "grpTime";
+            this.grpTime.Padding = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.grpTime.Size = new System.Drawing.Size(213, 337);
+            this.grpTime.TabIndex = 1;
+            this.grpTime.TabStop = false;
+            this.grpTime.Text = "Elasped Time";
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(58, 285);
+            this.btnExit.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(86, 31);
+            this.btnExit.TabIndex = 14;
+            this.btnExit.Text = "E&xit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // btnPause
+            // 
+            this.btnPause.Location = new System.Drawing.Point(58, 225);
+            this.btnPause.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnPause.Name = "btnPause";
+            this.btnPause.Size = new System.Drawing.Size(86, 31);
+            this.btnPause.TabIndex = 13;
+            this.btnPause.TabStop = false;
+            this.btnPause.Text = "&Pause";
+            this.btnPause.UseVisualStyleBackColor = true;
+            this.btnPause.Click += new System.EventHandler(this.btnPause_Click);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(58, 164);
+            this.btnStart.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(86, 31);
+            this.btnStart.TabIndex = 12;
+            this.btnStart.Text = "&Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // txtElapsedTime
+            // 
+            this.txtElapsedTime.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.txtElapsedTime.Location = new System.Drawing.Point(30, 59);
+            this.txtElapsedTime.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtElapsedTime.Name = "txtElapsedTime";
+            this.txtElapsedTime.Size = new System.Drawing.Size(151, 39);
+            this.txtElapsedTime.TabIndex = 10;
+            this.txtElapsedTime.TabStop = false;
+            this.txtElapsedTime.Text = "00:00:00";
+            this.txtElapsedTime.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // timSeconds
+            // 
+            this.timSeconds.Interval = 1000;
+            this.timSeconds.Tick += new System.EventHandler(this.timSeconds_Tick);
+            // 
+            // frmMailingList
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(914, 445);
+            this.Controls.Add(this.grpTime);
+            this.Controls.Add(this.grpMail);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "frmMailingList";
+            this.Text = "Mailing List Application";
+            this.grpMail.ResumeLayout(false);
+            this.grpMail.PerformLayout();
+            this.grpTime.ResumeLayout(false);
+            this.grpTime.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox grpMail;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnAccept;
+        private System.Windows.Forms.TextBox txtZip;
+        private System.Windows.Forms.TextBox txtState;
+        private System.Windows.Forms.TextBox txtCity;
+        private System.Windows.Forms.TextBox txtAddress;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox grpTime;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnPause;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.TextBox txtElapsedTime;
+        private System.Windows.Forms.Timer timSeconds;
+    }
+}
+
---
+++ MailingListApp/Form1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MailingListApp
+{
+    public partial class frmMailingList : Form
+    {
+        DateTime lastNow;
+        TimeSpan elapsedTime;
+        public frmMailingList()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            // Start button clicked
+            // Disable start and exit buttons
+            // Enabled pause button
+            
+            btnStart.Enabled = false;
+            btnExit.Enabled = false;
+            btnPause.Enabled = true;
+
+            // Established start time and start timer control
+
+            lastNow = DateTime.Now;
+            timSeconds.Enabled = true;
+
+            // Enabled mailing list frame
+
+            grpMail.Enabled = true;
+            txtName.Focus();
+        }
+
+        private void btnPause_Click(object sender, EventArgs e)
+        {
+            // Pause button clicked
+            // Disable pause button
+            // Enabled start and exi buttons
+
+            btnPause.Enabled = false;
+            btnStart.Enabled = true;
+            btnExit.Enabled = false;
+
+            // Stop timer
+
+            timSeconds.Enabled = false;
+
+            // Disable editing frame
+
+            grpMail.Enabled = false;
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void timSeconds_Tick(object sender, EventArgs e)
+        {
+            // Compute elapsed time and display
+
+            elapsedTime += DateTime.Now - lastNow;
+            txtElapsedTime.Text = Convert.ToString(new TimeSpan(elapsedTime.Hours, elapsedTime.Minutes, elapsedTime.Seconds));
+            lastNow = DateTime.Now;
+        }
+
+        private void txtInput_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            String boxName = ((TextBox)sender).Name;
+             
+            // Check for the return key
+
+            if ((int) e.KeyChar == 13)
+            {
+                switch (boxName)
+                {
+                    case "txtName":
+                        txtAddress.Focus();
+                        break;
+                    case "txtAddress":
+                        txtCity.Focus();
+                        break;
+                    case "txtCity":
+                        txtState.Focus();
+                        break;
+                    case "txtState":
+                        txtZip.Focus();
+                        break;
+                    case "txtZip":
+                        btnAccept.Focus();
+                        break;
+                }
+            }
+
+            // In zip text box, make sure only numbers or backspace pressed 
+
+            if (boxName.Equals("txtZip"))
+            {
+                if ((e.KeyChar >= '0' && e.KeyChar <= '9') || (int) e.KeyChar == 8)
+                {
+                    e.Handled = false;
+                }
+                else
+                {
+                    e.Handled = true;
+                }
+            }
+        }
+
+        private void btnAccept_Click(object sender, EventArgs e)
+        {
+            string s;
+
+            // Accept button clicked - form label and output is mesage box
+            // Make sure each text box has entry
+
+            if (txtName.Text.Equals("") || txtAddress.Text.Equals("") || txtCity.Text.Equals("") || txtState.Text.Equals("") || txtZip.Text.Equals(""))
+            {
+                MessageBox.Show("Each box must hav an entry!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            s = txtName.Text + "\r\n" + txtAddress.Text + "\r\n";
+            s += txtCity.Text + "," + txtState.Text + " " + txtZip.Text;
+            MessageBox.Show(s, "Mailing Label", MessageBoxButtons.OK);
+            btnClear.PerformClick();
+         }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtName.Text = "";
+            txtAddress.Text = "";
+            txtCity.Text = "";
+            txtState.Text = "";
+            txtZip.Text = "";
+            txtName.Focus();
+        }
+    }
+}
